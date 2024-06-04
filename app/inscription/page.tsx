adb9d723c7e8b1596c6ac779cf89cc1ec74812df
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-06-04 14:58:45 +0000

    Add Login Page

--- app/inscription/page.tsx
+++ app/inscription/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function page() {
   return (
     <div className="bg-custom-bg bg-cover bg-center h-screen">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -10,7 +10,17 @@ export default function Home() {
           </h1>
           <div className=" bg-white px-12 py-12 rounded-[8px]">
             <form action="" className="space-y-6">
-              <h3>Connectez-vous en tant que admin</h3>
+              <h3>Inscrivez-vous en tant que Admin</h3>
+              <div className="block">
+                <label htmlFor="" className="block">
+                  Nom
+                </label>
+                <input
+                  type="text"
+                  required
+                  className="border border-gray-500 rounded-[5px] py-1"
+                />
+              </div>
               <div className="block">
                 <label htmlFor="" className="block">
                   Email
@@ -29,10 +39,7 @@ export default function Home() {
                   className="border border-gray-500 rounded-[5px] py-1"
                 />
               </div>
-              <div className="space-x-2">
-                <input type="checkbox" />
-                <label htmlFor="">Gardez-moi connecte</label>
-              </div>
+
               <div className="flex justify-center mt-2">
                 <button className="bg-[#45484B] font-bold text-white px-[5.8rem] py-2">
                   Se Connecter
@@ -41,13 +48,10 @@ export default function Home() {
             </form>
           </div>
           <div className="text-center pt-4">
-            <Link href="/reset-passworld" className="text-[#FFD964] block">
-              Mot de passe oublie ?
-            </Link>
             <span className="mt-10 text-white">
-              Vous n'avez pas de compte ?{" "}
-              <Link href="/inscription" className="text-[#FFD964]">
-                S'inscrire
+              Vous avez un compte ?{" "}
+              <Link href="/" className="text-[#FFD964]">
+                Se connecter
               </Link>
             </span>
           </div>
